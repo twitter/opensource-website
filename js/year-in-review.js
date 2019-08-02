920fe5a5875e140e718070d3e89996a028b5a633
Jennifer Xie <xjenniferxie@gmail.com>
2019-08-02 13:07:16 -0700

    top 10 repo data now comes from repo_commits.json

--- js/year-in-review.js
+++ js/year-in-review.js
@@ -13,3 +13,40 @@ $('a[href*=#]:not([href=#])').click(function() {
         }
     }
 });
+
+// Insert top 10 repos
+
+var renderTopRepos = function(reposList) {
+    let topTenRepos = reposList.slice(0, 10);
+    var topTenReposTable = document.getElementById("top-10-repos");
+    var num = 1;
+
+    for (var repo of topTenRepos) {
+        // Row
+        var row = document.createElement('tr');
+        row.className = "content";
+
+        // Number
+        var numData = document.createElement('td');
+        numData.className = "num";
+        numData.innerHTML = num.toString();
+        num++;
+        row.appendChild(numData);
+
+        // Repo name
+        var repoData = document.createElement('td');
+        repoData.className = "repo";
+        repoData.innerHTML = repo.name;
+        row.appendChild(repoData);
+
+        // Commit count
+        var commitCountData = document.createElement('td');
+        commitCountData.className = "commit-value";
+        commitCountData.innerHTML = repo.commitCount;
+        row.appendChild(commitCountData);
+
+        topTenReposTable.appendChild(row);
+    }
+}
+
+renderTopRepos(allRepos);
